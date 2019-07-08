135cfc516c828fb09a2fd7ca761482e1051ee442
MarcusJP <javin.marc@gmail.com>
2019-07-08 17:10:03 +0200

    comments main

--- main.m
+++ main.m
@@ -1,21 +1,26 @@
 
 DT = 0.1;
 
+% weight matrix
 W = [0 1 0; 
     -1 0 1; 
     0.2 -1.5 0];
 
 
+%neural model and parameters
 params.neurfunc = @leakint;
 params.neur.E = 0;
 params.neur.G = 1;
 params.neur.Cm = 1;
+
+%synaptic model and parameters
 params.synfunc = @synrate;
 params.syn.R0 = 5;
 params.syn.RMAX = 100;
 
-
+%computation
 out = calculate(W, [], [], DT, params);
 
+%plotting
 figure(1)
 plot(out)
