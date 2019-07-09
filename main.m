143b41f3017bee561c3de453bd13e122f1b3b734
MarcusJP <javin.marc@gmail.com>
2019-07-09 17:03:56 +0200

    axplanation main

--- main.m
+++ main.m
@@ -8,39 +8,42 @@ Ws = [0   1.4  0;
      0.2 -1.5  0];
 Wg = 0;
 
-%get weights from correlation
+%% get weights from correlation
 CONNS = readtable('data/corr_W.csv');
 Ws = csvread('data/corr_W.csv', 1, 1);
 
-%neural model and parameters
+%% neural model and parameters
 params.neur.func = @leakint;
 params.neur.E = 0;
 params.neur.G = 1;
 params.neur.Cm = 1;
 
-%rate synaptic model and parameters
+%% use rate synaptic model
 params.syn.func = @synrate;
 params.syn.R0 = 5;
 params.syn.RMAX = 100;
 
-% %sigmoidal synaptic model
+%% use sigmoidal synaptic model
 % params.syn.func = @synsig;
 % params.syn.E = 10;
 % params.syn.MDP = 5;
 % params.syn.SCALE = 20;
 
-% weight matrix
+%% synaptic weight matrix
 params.syn.W = Ws;
             
-%gap junction model and parameters
+%% gap junction model and parameters
 params.gap.func = @gap;
 params.gap.W = Wg;
 
-%computation
+%% computation
+% initial activity
 INIT = [];
+% injected current ([n_time, n_neuron])
 INJ = ones(100, size(Ws,1));
+%function call
 out = calculate(INIT, INJ, DT, params);
 
-%plotting
+%% plotting
 figure()
 plot(out)
